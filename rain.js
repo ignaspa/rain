2b79b3ca5abacd1c759260d642e8292c5492c84c
ignaspa <ignaspan@gmail.com>
2018-01-23 20:32:56 -0500

    most done

--- rain.js
+++ rain.js
@@ -6,6 +6,8 @@ const colorlibrary = ["#379b54", "#d9ff51", "#006caf", "#ffb7ef", "#ffa89b"
 , "#b21700", "#b3efc8", "#5f308e", "#085166"];
 const size_inflation = 5;
 
+var begin = false;
+var puddles = [];
 
 
 
@@ -32,7 +34,12 @@ var animate = window.requestAnimationFrame ||
   function(callback) { window.setTimeout(callback, 1000/60) };
 
 
+  var step = () => {
 
+    update()
+
+    render()
+  }
 
 
 
@@ -44,20 +51,27 @@ class Puddle {
     this.x = x;
     this.y = y;
     this.radius = initial_R;
-
+    this.middle = false;
   }
 
   render() {
     context.beginPath();
-    context.fillstyle = color;
+    context.fillstyle = smallcolor;
     context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
     context.fill()
+    if (this.middle){
+      context.fillstyle = bigcolor;
+      context.arc(this.x, this.y, this.radius - edge , 0, 2 * Math.PI)
+      context.fill()
+    }
   }
 
 
  update(){
-
-
+   if (this.radius == edge_point){
+     this.middle = true;
+   }
+   this.radius += 2;
 
  }
 }
@@ -66,18 +80,9 @@ class Puddle {
 
 
 
-var step = () => {
-
-  update()
-
-  render()
-}
-
-}
-
 window.addEventListener("keydown",
   function(event){
-     if (event.keyCode == 65){
+     if (event.keyCode == 71){
        begin = true
      }
 
@@ -105,16 +110,12 @@ var update = () => {
   for (i = 0; i < puddles.length; i++) {
     puddles[i].update();
   }
+  if (begin) {
+    puddles.push(new Puddle(Math.random()*width + 1, Math.random()*height + 1))
+  }
+
 
 }
-
-
-
-
-var playerOne = new Paddle(canvas.width/2 - PADDLE_WIDTH/2,0, "topL", "topR")
-var playerTwo = new Paddle(canvas.width/2- PADDLE_WIDTH/2, height - PADDLE_HEIGHT, "botL", "botR")
-var ballOne = new Ball(0.5 * canvas.width, 0.5 * canvas.height)
-
 
 
 
@@ -122,12 +123,32 @@ var render = () => {
   context.fillStyle = "#000000"
   context.fillRect(0,0, width, height)
 
-  if (begin == true){
+
+
+  if (begin == false){
+    context.font = "25px Courier New";
+    context.fillStyle = "orange";
+    context.textAlign = "center"
+    context.fillText("Press [g] // Pousser [g]", 0.5 * canvas.width, 0.5 * canvas.height);
+
+  }
+
+
+  if (begin){
+    var bigcolor = colorlibrary[(Math.random() * colorlibrary.length) + 1];
+    context.fillStyle = bigcolor
+    context.fillRect(0,0, width, height)
+    delete colorlibrary[indexOf(bigcolor)];
+    var smallcolor = colorlibrary.[(Math.random() * colorlibrary.length) + 1];
+
+    for (i = 0; i < puddles.length; i++) {
+      puddles[i].render();
+    }
+
 
 
   }
 
 
 
-}
 }
